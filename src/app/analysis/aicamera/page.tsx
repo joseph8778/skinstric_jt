6e71f7eb7c84f96b3b1c16e4b6d6d02975324d1a
Joseph <joseph8778@gmail.com>
2025-03-28 13:50:04 -0500

    created actionButton component, added image file capture function, successfully pushed image file to demographics api

--- src/app/analysis/aicamera/page.tsx
+++ src/app/analysis/aicamera/page.tsx
@@ -8,10 +8,15 @@ import { useEffect, useRef, useState } from "react";
 import { useTheme } from "next-themes";
 import BetterResultsDisclaimer from "@/components/BetterResultsDisclaimer";
 import cameraIcon from '@/assets/CameraIcon.svg'
+import ActionButton from "@/components/ActionButton";
+import { blob } from "stream/consumers";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export default function AiCameraPage() {
     const videoRef = useRef<HTMLVideoElement>(null);
     const videoBlurRef = useRef<HTMLVideoElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
     const [videoerror, setVideoError] = useState<string | null>(null);
     const [loading, setLoading] = useState(true);
     const {theme, setTheme } = useTheme()
@@ -54,7 +59,82 @@ export default function AiCameraPage() {
     //     console.log('videoRef.current:', videoRef.current);
     // }, [loading, videoerror, videoRef.current]);
 
-     
+
+    const handleTakeImage = () => {
+        if (videoRef.current && canvasRef.current) {
+            const canvas = canvasRef.current;
+            const context = canvas.getContext('2d');
+            if (context) {
+                canvas.width = videoRef.current.videoWidth;
+                canvas.height = videoRef.current.videoHeight;
+
+                context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+
+                const imageData = canvas.toDataURL('image/png');
+                console.log('Captured image data URL:', imageData);
+
+                const base64Data = imageData.split(',')[1];
+                console.log('Base64 image data:', base64Data);
+
+                canvas.toBlob((blob) => {
+                if (blob) {
+                    const file = new File([blob], 'captured-image.png', { type: 'image/png' });
+                    console.log('Captured image:', file)
+                    getPhotoData(file)
+                    }
+                }, 'iamge/png');
+
+            }
+        }
+    }
+
+    const router = useRouter()
+    async function getPhotoData(selectedPhoto: File | null) {
+        if (selectedPhoto) {
+        //   animRef.current?.forEach((animation) =>  {
+        //     animation.reverse()
+        //   })
+        //   setTimeout(() => {
+        //     setpageLoader(true)
+        //   }, 1700);
+        
+          
+          const reader = new FileReader();
+          reader.onloadend = async () => {
+            const base64Image = reader.result?.toString().split(',')[1];  
+    
+            if (base64Image) {
+             
+              try {
+                const response = await axios.post(
+                  'https://us-central1-frontend-simplified.cloudfunctions.net/skinstricPhaseTwo',
+                  { image: base64Image },
+                  {
+                    headers: {
+                      "Content-Type": 'application/json',  
+                    },
+                  }
+                );
+               
+                console.log(response.data.data);
+                localStorage.setItem('DemoData', JSON.stringify(response.data.data))
+                router.push(`/analysis/demographics`)
+                
+              } catch (error) {
+                if (axios.isAxiosError(error)) {
+                  console.log('Error Response:', error)
+                    setVideoError('An error occurred while processing the image. Please try again.');
+                } else {
+                  console.log('Unexpected Error:', error);
+                }
+              }
+            }
+          };
+          
+          reader.readAsDataURL(selectedPhoto);
+        }
+      }
+
     return (
         <>
         {loading ? ( 
@@ -87,24 +167,14 @@ export default function AiCameraPage() {
             <main > 
                 <video ref={videoBlurRef} autoPlay playsInline className="absolute top-0 left-0 w-full h-full object-cover blur-sm "></video>
                 <video ref={videoRef} autoPlay playsInline  className="absolute top-0 left-0 w-full h-full object-cover [mask-image:radial-gradient(ellipse_25%_80%,#000_45%,transparent_55%)] z-0" />
-                <div className="group absolute
-                top-1/2 right-0 -translate-x-[10%] -translate-y-[50%]
-                flex justify-center items-center gap-5"> 
-                    <h1 className="z-10 dark:text-white font-semibold group-hover:-translate-x-[15%] transition-all ease-in-out duration-300">TAKE PICTURE</h1>
-                        <div className="z-10 relative rounded-full bg-white size-16 flex justify-center items-center hover:cursor-pointer group-hover:scale-[1.2] transition-all duration-300 ease-in-out">
-                            <div className="z-11 rounded-full border-2 border-gray-400 size-[95%] absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] ">
-                                <Image src={cameraIcon} className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]' alt="cameraIcon"/>
-                            </div>
-                        </div>
-                </div>
+         <ActionButton onClick={() => handleTakeImage()} icon={cameraIcon}></ActionButton>
+        <canvas ref={canvasRef} className="hidden"></canvas>
                 <div className="flex justify-center">
                 <BetterResultsDisclaimer parentProps="static bottom-[30]"/>
                 </div>
             </main>
             <footer >
                 <NavBtn direction="left" routerLink="/testing"></NavBtn>
-            
-
             </footer>
             </>
             )  
---
+++ src/components/ActionButton.tsx
@@ -0,0 +1,25 @@
+import Image, { StaticImageData } from 'next/image';
+
+type ActionButtonProps = {
+    text?: string;
+    onClick: () => void;
+    icon?: string | StaticImageData;
+    iconAlt?: string;
+}
+
+export default function ActionButton({ text = 'Take a picture', onClick, icon = '@/assets/CameraIcon.svg', iconAlt = 'Camera Icon' }: ActionButtonProps) {
+    return (
+        <div className="group absolute
+                top-1/2 right-0 -translate-x-[10%] -translate-y-[50%]
+                flex justify-center items-center gap-5"
+                onClick={onClick}
+                > 
+                    <h1 className="z-10 dark:text-white font-semibold group-hover:-translate-x-[15%] transition-all ease-in-out duration-300">{text.toUpperCase()}</h1>
+                        <div className="z-10 relative rounded-full bg-white size-16 flex justify-center items-center hover:cursor-pointer group-hover:scale-[1.2] transition-all duration-300 ease-in-out">
+                            <div className="z-11 rounded-full border-2 border-gray-400 size-[95%] absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] ">
+                                {icon && <Image src={icon} className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]' alt={iconAlt}/>}
+                            </div>
+                        </div>
+                </div>
+    )
+}
